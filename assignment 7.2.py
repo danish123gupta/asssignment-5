20eb1d8148061364832616536cbbd1b773018853
danish123gupta <gupta.danish236@gmail.com>
2018-06-21 20:18:52 +0530

    first commit

---
+++ assignment 7.2.py
@@ -0,0 +1,10 @@
+import datetime
+from datetime import date
+print(date.today())
+
+import os
+print(os.getcwd())
+
+
+import os
+print(os.environ)
---
+++ assignment 7.3py.py
@@ -0,0 +1,65 @@
+#q.no.1
+print("the time tuple returned by localtime and gmtime contains year,month,day,hour,minuteand so on")
+
+#q.no.2
+
+import time
+localtime = time.asctime(time.localtime(time.time()))
+print("local current time:",localtime)
+
+
+
+#q.no.3
+
+
+import datetime
+i = datetime.datetime.now()
+print("current month = %s" %i.month)
+
+
+#q.no.4
+import datetime
+i = datetime.datetime.now()
+print("current day = %s" %i.day)
+
+
+
+
+#q.no.5
+import datetime
+mydate= datetime.date(2021,1,11)
+print(mydate.strftime("%A"))
+
+
+#Q.NO.6
+import time
+localtime = time.localtime(time.time())
+print("local current time:",localtime)
+
+
+#q.no.7
+import math
+n=int(input("enter number:"))
+print (math.factorial(n))
+
+#q.no.8
+import math
+n=int(input("enter number:"))
+m=int(input("enter number:"))
+print (math.gcd(n ,m))
+
+
+#q.no.9
+import os
+print(os.getcwd())
+print(os.environ)
+
+
+
+
+
+
+
+
+
+
